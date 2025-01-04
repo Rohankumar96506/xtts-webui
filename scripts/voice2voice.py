9eaaf1a89c25de1798e5c3fb2eae53d3817c8060
Rohankumar96506 <59435105+Rohankumar96506@users.noreply.github.com>
2025-01-04 14:35:38 +0530

    Update voice2voice.py

--- scripts/voice2voice.py
+++ scripts/voice2voice.py
@@ -80,6 +80,7 @@ def infer_rvc(pitch, index_rate, protect_voiceless, method, index_path, model_pa
     try:
         cmd = [
             'python', '-m', 'rvc_python',
+            '-h', 'cli',
             '--input', input_path,
             '--model', index_path,
             '--pitch', str(pitch),
